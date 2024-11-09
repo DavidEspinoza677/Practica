50fa0b8e656e1aa9c16a8bfa48342ce3b0a96d62
DavidEspinoza677 <davespinoza@uamv.edu.ni>
2024-11-08 19:27:28 -0600

    Ejercicios practicos 08112024

---
+++ Practica/Practica/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxResultados = new System.Windows.Forms.ListBox();
+            this.cmbEjercicios = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxResultados
+            // 
+            this.listBoxResultados.FormattingEnabled = true;
+            this.listBoxResultados.Location = new System.Drawing.Point(331, 55);
+            this.listBoxResultados.Name = "listBoxResultados";
+            this.listBoxResultados.Size = new System.Drawing.Size(457, 277);
+            this.listBoxResultados.TabIndex = 3;
+            // 
+            // cmbEjercicios
+            // 
+            this.cmbEjercicios.FormattingEnabled = true;
+            this.cmbEjercicios.Location = new System.Drawing.Point(91, 71);
+            this.cmbEjercicios.Name = "cmbEjercicios";
+            this.cmbEjercicios.Size = new System.Drawing.Size(218, 21);
+            this.cmbEjercicios.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selecione Ejercicio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ingrese Numeros o Nombres ";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(91, 122);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(218, 20);
+            this.txtInput.TabIndex = 8;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(91, 148);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(218, 23);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar Datos";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click_1);
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(91, 177);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(218, 23);
+            this.btnEjecutar.TabIndex = 10;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(91, 206);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(218, 23);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbEjercicios);
+            this.Controls.Add(this.listBoxResultados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBoxResultados;
+        private System.Windows.Forms.ComboBox cmbEjercicios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Practica/Practica/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Form1 : Form
+    {
+        private List<int> numeros = new List<int>();
+        private List<string> nombres = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += MainForm_Load;
+
+            btnAgregar.Click += btnAgregar_Click;
+            btnEjecutar.Click += btnEjecutar_Click;
+            btnLimpiar.Click += btnLimpiar_Click;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            cmbEjercicios.Items.Add("Ejercicio 1: Números pares e impares");
+            cmbEjercicios.Items.Add("Ejercicio 2: Nombres que empiezan con 'J'");
+            cmbEjercicios.SelectedIndex = 0;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string selectedExercise = cmbEjercicios.SelectedItem?.ToString();
+            string input = txtInput.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Por favor, ingrese un dato válido.");
+                return;
+            }
+
+            if (selectedExercise == "Ejercicio 1: Números pares e impares")
+            {
+                if (int.TryParse(input, out int numero))
+                {
+                    numeros.Add(numero);
+                    txtInput.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese un número válido.");
+                }
+            }
+            else if (selectedExercise == "Ejercicio 2: Nombres que empiezan con 'J'")
+            {
+                if (input.Length >= 10 && input.Length <= 60)
+                {
+                    nombres.Add(input);
+                    txtInput.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("El nombre debe tener entre 10 y 60 letras.");
+                }
+            }
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            listBoxResultados.Items.Clear();
+            string selectedExercise = cmbEjercicios.SelectedItem?.ToString();
+
+            if (selectedExercise == "Ejercicio 1: Números pares e impares")
+            {
+                EjecutarEjercicio1();
+            }
+            else if (selectedExercise == "Ejercicio 2: Nombres que empiezan con 'J'")
+            {
+                EjecutarEjercicio2();
+            }
+        }
+
+        private void EjecutarEjercicio1()
+        {
+            List<int> numerosPares = new List<int>();
+            List<int> numerosImpares = new List<int>();
+
+            foreach (var numero in numeros)
+            {
+                if (numero % 2 == 0)
+                    numerosPares.Add(numero);
+                else
+                    numerosImpares.Add(numero);
+            }
+
+            listBoxResultados.Items.Add("Números pares: " + string.Join(", ", numerosPares));
+            listBoxResultados.Items.Add("Números impares: " + string.Join(", ", numerosImpares));
+        }
+
+        private void EjecutarEjercicio2()
+        {
+            List<string> nombresConJ = new List<string>();
+            List<string> otrosNombres = new List<string>();
+
+            foreach (var nombre in nombres)
+            {
+                if (nombre.StartsWith("J", StringComparison.OrdinalIgnoreCase))
+                    nombresConJ.Add(nombre);
+                else
+                    otrosNombres.Add(nombre);
+            }
+
+            listBoxResultados.Items.Add("Nombres con 'J': " + string.Join(", ", nombresConJ));
+            listBoxResultados.Items.Add("Otros nombres: " + string.Join(", ", otrosNombres));
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            numeros.Clear();
+            nombres.Clear();
+            txtInput.Clear();
+            listBoxResultados.Items.Clear();
+            MessageBox.Show("Datos y resultados limpiados.");
+        }
+
+        private void btnAgregar_Click_1(object sender, EventArgs e)
+        {
+            string selectedExercise = cmbEjercicios.SelectedItem?.ToString();
+            string input = txtInput.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Por favor, ingrese un dato válido.");
+                return;
+            }
+
+            if (selectedExercise == "Ejercicio 1: Números pares e impares")
+            {
+                if (int.TryParse(input, out int numero))
+                {
+                    numeros.Add(numero);
+                    txtInput.Clear();
+                    txtInput.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Ingrese un número válido.");
+                }
+            }
+            else if (selectedExercise == "Ejercicio 2: Nombres que empiezan con 'J'")
+            {
+                if (input.Length <= 60)  
+                {
+                    nombres.Add(input);
+                    txtInput.Clear();
+                    txtInput.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("El nombre no debe exceder las 60 letras.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, seleccione un ejercicio.");
+            }
+        }
+    }
